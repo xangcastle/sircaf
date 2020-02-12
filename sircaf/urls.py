6a5e23f57721b499f372b639fec7016aded90a8a
xangcastle <xangcastle@gmail.com>
2020-02-12 00:14:38 -0600

    change urls

--- sircaf/urls.py
+++ sircaf/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('grappelli/', include('grappelli.urls')),
     path('admin/ajax/', include('grappelli_extras.ajax_urls')),
 ]
